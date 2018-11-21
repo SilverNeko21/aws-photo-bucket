d28092974bb0a430f19b63d5261bbbbe003b699f
SilverNeko21 <larong2122@gmail.com>
2018-11-21 13:49:35 -1000

    oof of death

---
+++ listObjects.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
+
+module.exports.list = (event, context, callback) => {
+  const params = {
+    Bucket: "treasure-chest-capstone"
+  };
+  s3.listObjects(params, function(err, data) {
+    if (err) console.log(err, err.stack); 
+    console.log('data', data);
+   
+    const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      message: data
+    })
+  };
+  callback(null, response)
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  })
+};
+  
