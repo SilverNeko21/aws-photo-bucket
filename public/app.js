78944fc4bf67f1ee04960a17e4670dd6f09842c1
SilverNeko21 <larong2122@gmail.com>
2018-11-21 14:38:56 -1000

    oof2

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
-const result = document.querySelector('#result');
-
+const oof= document.querySelector('#butt')
+oof.addEventListener("click", event =>{
 axios
-  .get('https://dq0ae9tuv4.execute-api.us-west-2.amazonaws.com/dev/skipper/')
+  .get('https://dq0ae9tuv4.execute-api.us-west-2.amazonaws.com/dev/skipper')
   .then(response => {
     htmlTemplateBuilder(response);
     console.log(response)
@@ -9,13 +9,16 @@ axios
   .catch(error => {
     console.log('error', error);
   });
+});
+//const result = document.querySelector('#result');
+
 
 const htmlTemplateBuilder = response => {
     console.log('response', response);
   result.innerHTML = response.data.message.Contents
-    .map(pics => {
+    .map(pic => {
       return `
-      <img src="https://s3-us-west-2.amazonaws.com/treasure-chest-capstone/${pics.key}">
+      <img src="https://s3-us-west-2.amazonaws.com/treasure-chest-capstone/${pic.Key}">
       `;
     })
     .join('');
